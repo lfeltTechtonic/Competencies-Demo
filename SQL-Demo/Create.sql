d65acc97fbd92230efbede0249cd6d73a31c6125
larissa <larissafelt@techtonic.com>
2019-05-29 11:30:13 -0600

    Create and Insert

---
+++ SQL-Demo/Create.sql
@@ -0,0 +1,6 @@
+CREATE TABLE cats (
+    Name TEXT,
+    Breed TEXT,
+    Color TEXT,
+    Age INTEGER,
+);
---
+++ SQL-Demo/Insert.sql
@@ -0,0 +1,37 @@
+INSERT INTO cats  
+    ([Name],[Breed],[Color],[Age])
+    VALUES
+   ('Freyja', 'Russian Blue',  'Grey', 10)
+
+GO
+
+INSERT INTO cats  
+    ([Name],[Breed],[Color],[Age])
+    VALUES
+   ('Jay', 'Main Coone',  'Black', 14),
+   ('Mika', 'Mix',  'Grey', 3),
+   ('Benji', 'Ginger',  'Orange', 1),
+   ('Alvin', 'Tabby',  'Orange', 4),
+   ('Theodosia', 'Mix',  'Black', 1),
+   ('Willow', 'Tortoise Shell',  'Brown', 13)
+
+
+GO
+
+INSERT INTO cats  
+    ([Name],[Breed],[Color],[Age])
+    VALUES
+   ('Sage', 'Fluffy',  'Grey', 8),
+    ('Buck', 'Tuxedo',  'Black and White', 10),
+    ('Batmin', 'Tuxedo',  'Black and White', 9),
+    ('Sushi', 'Mutt',  'White, Black and Brown', 1)
+GO
+
+INSERT INTO cats  
+    ([Name],[Breed],[Color],[Age])
+    VALUES
+   ('Rosie', 'Mix',  'White, Black and Brown', 3),
+   ('Oliva Benson', 'Scottish Fold',  'White', 6),
+   ('Merdith Grey', 'Scottish Fold',  'White and Grey', 5)
+   
+GO
